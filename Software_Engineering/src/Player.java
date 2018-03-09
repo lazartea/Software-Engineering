d21523eaf16fca841aad692b6588bbf9d1bb5719
HarryFossey <bazfossey@hotmail.com>
2018-03-09 00:21:23 +0000

    Updating Player

--- Software_Engineering/src/Player.java
+++ Software_Engineering/src/Player.java
@@ -1,13 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Harry
- */
+
 public class Player {
     
+    
+    private int playerCash;
+    
+    public Player()
+    {
+        this.playerCash = 1500;
+    }
+    
+    public int getCash()
+    {
+        return playerCash;
+    }
+    
+    public void changePlayerCash(int changeBy)
+    {
+        playerCash += changeBy;
+        if(playerCash <= 0)
+        {
+            //Check if player can sell assets, otherwise player loses.
+        }
+    }    
 }
