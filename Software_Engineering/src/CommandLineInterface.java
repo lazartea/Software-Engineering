80de0eb95567bb94f6421c3bfe9900efe94b713e
lazartea <lazartea@reed.edu>
2018-05-10 16:10:37 +0100

    Update CommandLineInterface.java
    add roll again functionality

--- Software_Engineering/src/CommandLineInterface.java
+++ Software_Engineering/src/CommandLineInterface.java
@@ -230,19 +230,19 @@ class CommandLineInterface implements UserInterface {
     
     @Override 
     public int getTurnOption() {
-        System.out.println("1:End Turn  2:Buy property 3:Display Property Data"); //More to be added
+        System.out.println("1:End Turn  2:Buy property 3:Display Property Data 4:Roll Again"); //More to be added
         while (true) {
             try {
                 Scanner scanner = new Scanner(System.in);
                 int option = scanner.nextInt();
-                if (option <= 4) {
+                if (option <= 5) {
                     return option;
                 } else {
-                    System.out.println("Type 1-3");
+                    System.out.println("Type 1-4");
                 }
                 
             } catch (java.util.InputMismatchException e) {
-                System.out.println("Type 1-3");
+                System.out.println("Type 1-4");
             }  
     }
     }
